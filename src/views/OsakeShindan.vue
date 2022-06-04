70320a3d0ccb0d419e60edd00cc4dc0d72c34d81
ShomaOchi <minamimachida0617@gmail.com>
2022-06-04 18:21:50 +0900

    お酒診断の大枠の変更

--- src/views/OsakeShindan.vue
+++ src/views/OsakeShindan.vue
@@ -14,7 +14,103 @@
   </div>
   <div class="finish" v-if="finishshow">
     <h1>あなたのオススメは…</h1>
-    <div class="result">{{ result[count] }}</div>
+    <!-- 結果をここに表示 -->
+    <div class="result" v-if="this.string === '高甘柑橘炭酸和'">
+      {{ this.result[0] }}
+    </div>
+    <div class="result" v-if="this.string === '高甘柑橘炭酸洋'">
+      {{ this.result[1] }}
+    </div>
+    <div class="result" v-if="this.string === '高甘柑橘和'">
+      {{ this.result[2] }}
+    </div>
+    <div class="result" v-if="this.string === '高甘柑橘洋'">
+      {{ this.result[3] }}
+    </div>
+    <div class="result" v-if="this.string === '高甘炭酸和'">
+      {{ this.result[4] }}
+    </div>
+    <div class="result" v-if="this.string === '高甘炭酸洋'">
+      {{ this.result[5] }}
+    </div>
+    <div class="result" v-if="this.string === '高甘和'">
+      {{ this.result[6] }}
+    </div>
+    <div class="result" v-if="this.string === '高甘洋'">
+      {{ this.result[7] }}
+    </div>
+    <div class="result" v-if="this.string === '高辛柑橘炭酸和'">
+      {{ this.result[8] }}
+    </div>
+    <div class="result" v-if="this.string === '高辛柑橘炭酸洋'">
+      {{ this.result[9] }}
+    </div>
+    <div class="result" v-if="this.string === '高辛柑橘和'">
+      {{ this.result[10] }}
+    </div>
+    <div class="result" v-if="this.string === '高辛柑橘洋'">
+      {{ this.result[11] }}
+    </div>
+    <div class="result" v-if="this.string === '高辛炭酸和'">
+      {{ this.result[12] }}
+    </div>
+    <div class="result" v-if="this.string === '高辛炭酸洋'">
+      {{ this.result[13] }}
+    </div>
+    <div class="result" v-if="this.string === '高辛和'">
+      {{ this.result[14] }}
+    </div>
+    <div class="result" v-if="this.string === '高辛洋'">
+      {{ this.result[15] }}
+    </div>
+    <div class="result" v-if="this.string === '低甘柑橘炭酸和'">
+      {{ this.result[16] }}
+    </div>
+    <div class="result" v-if="this.string === '低甘柑橘炭酸洋'">
+      {{ this.result[17] }}
+    </div>
+    <div class="result" v-if="this.string === '低甘柑橘和'">
+      {{ this.result[18] }}
+    </div>
+    <div class="result" v-if="this.string === '低甘柑橘洋'">
+      {{ this.result[19] }}
+    </div>
+    <div class="result" v-if="this.string === '低甘炭酸和'">
+      {{ this.result[20] }}
+    </div>
+    <div class="result" v-if="this.string === '低甘炭酸洋'">
+      {{ this.result[21] }}
+    </div>
+    <div class="result" v-if="this.string === '低甘和'">
+      {{ this.result[22] }}
+    </div>
+    <div class="result" v-if="this.string === '低甘洋'">
+      {{ this.result[23] }}
+    </div>
+    <div class="result" v-if="this.string === '低辛柑橘炭酸和'">
+      {{ this.result[24] }}
+    </div>
+    <div class="result" v-if="this.string === '低辛柑橘炭酸洋'">
+      {{ this.result[25] }}
+    </div>
+    <div class="result" v-if="this.string === '低辛柑橘和'">
+      {{ this.result[26] }}
+    </div>
+    <div class="result" v-if="this.string === '低辛柑橘洋'">
+      {{ this.result[27] }}
+    </div>
+    <div class="result" v-if="this.string === '低辛炭酸和'">
+      {{ this.result[28] }}
+    </div>
+    <div class="result" v-if="this.string === '低辛炭酸洋'">
+      {{ this.result[29] }}
+    </div>
+    <div class="result" v-if="this.string === '低辛和'">
+      {{ this.result[30] }}
+    </div>
+    <div class="result" v-if="this.string === '低辛洋'">
+      {{ this.result[31] }}
+    </div>
   </div>
 </template>
 
@@ -23,82 +119,124 @@ export default {
   data() {
     return {
       questionnamber: Number(0),
-      count: 0,
+      string: "",
+      // スタート画面表示
       startshow: true,
+      // 診断画面表示
       questionshow: false,
+      // 結果画面表示
       finishshow: false,
       questions: [
-        { text: "お酒が得意だ" },
-        { text: "甘いお酒は苦手だ" },
-        { text: "フルーティーな味を好む" },
+        // 診断の質問内容
+        { text: "度数高いお酒が飲みたい！" },
+        { text: "甘口派だ" },
+        { text: "柑橘類が好きだ" },
+        { text: "炭酸が飲めない" },
+        { text: "和か洋かだったら和だ" },
       ],
       result: [
-        "梅酒",
-        "カシオレ",
-        "レモンサワー",
-        "お茶割",
-        "テキーラサンライズ",
-        "ワイン",
-        "ハイボール",
-        "ビール",
+        // 結果内容をここの配列に入れる
+        "高甘柑橘炭酸和の結果",
+        "高甘柑橘炭酸洋の結果",
+        "高甘柑橘炭酸和",
+        "高甘柑橘炭酸洋",
+        "高甘柑橘和",
+        "高甘柑橘洋",
+        "高甘炭酸和",
+        "高甘炭酸洋",
+        "高甘和",
+        "高甘洋",
+        "高辛柑橘炭酸和",
+        "高辛柑橘炭酸洋",
+        "高辛柑橘和",
+        "高辛柑橘洋",
+        "高辛炭酸和",
+        "高辛炭酸洋",
+        "高辛和",
+        "高辛洋",
+        "低甘柑橘炭酸和",
+        "低甘柑橘炭酸洋",
+        "低甘柑橘和",
+        "低甘柑橘洋",
+        "低甘炭酸和",
+        "低甘炭酸洋",
+        "低甘和",
+        "低甘洋",
+        "低辛柑橘炭酸和",
+        "低辛柑橘炭酸洋",
+        "低辛柑橘和",
+        "低辛柑橘洋",
+        "低辛炭酸和",
+        "低辛炭酸洋",
+        "低辛和",
+        "低辛洋",
       ],
     }
   },
   methods: {
+    // スタートボタンを押した時の動作
     startquestion: function () {
       this.startshow = false
       this.questionshow = true
     },
+    // 〇を押したときの動作
     nextquestion1: function () {
-      // １問目
+      // 1問目
       if (this.questionnamber === 0) {
-        this.questionnamber = Number(this.questionnamber) + 1
-        this.count = Number(this.count) + 7
+        this.string += "高"
+        console.log(this.string)
+        this.questionnamber += 1
       }
-      // ２問目
+      // 2問目
       else if (this.questionnamber === 1) {
-        this.questionnamber = Number(this.questionnamber) + 1
-      }
-      // ３問目(２問目に✕と答えたとき)
-      else if (this.questionnamber === 2 && this.count === 2) {
-        this.count = Number(this.count) - 1
-        this.questionshow = false
-        this.finishshow = true
-      } else if (this.questionnamber === 2 && this.count === 6) {
-        this.count = Number(this.count) - 1
-        this.questionshow = false
-        this.finishshow = true
-      } // ３問目(２問目に〇と答えたとき)
-      else {
+        this.string += "甘"
+        console.log(this.string)
+        this.questionnamber += 1
+      } // 3問目
+      else if (this.questionnamber === 2) {
+        this.string += "柑橘"
+        console.log(this.string)
+        this.questionnamber += 1
+      } // 4問目
+      else if (this.questionnamber === 3) {
+        this.string += "炭酸"
+        console.log(this.string)
+        this.questionnamber += 1
+      } // 5問目
+      else if (this.questionnamber === 4) {
+        this.string += "和"
+        console.log(this.string)
         this.questionshow = false
         this.finishshow = true
       }
     },
+    // ✕を押したときの動作
     nextquestion2: function () {
-      // 一問目
+      // 1問目
       if (this.questionnamber === 0) {
-        this.count = Number(this.count) + 3
-        this.questionnamber = Number(this.questionnamber) + 1
+        this.string += "低"
+        console.log(this.string)
+        this.questionnamber += 1
       }
-      // ２問目
+      // 2問目
       else if (this.questionnamber === 1) {
-        this.questionnamber = Number(this.questionnamber) + 1
-        this.count = Number(this.count) - 1
-      }
-
-      // ３問目(２問目に✕と答えたとき)
-      else if (this.questionnamber === 2 && this.count === 2) {
-        this.count = Number(this.count) - 2
-        this.questionshow = false
-        this.finishshow = true
-      } else if (this.questionnamber === 2 && this.count === 6) {
-        this.count = Number(this.count) - 2
-        this.questionshow = false
-        this.finishshow = true
-      }
-      // ３問目(２問目に〇と答えたとき)
-      else {
-        this.count = Number(this.count) - 1
+        this.string += "辛"
+        console.log(this.string)
+        this.questionnamber += 1
+      } // 3問目
+      else if (this.questionnamber === 2) {
+        this.string += ""
+        console.log(this.string)
+        this.questionnamber += 1
+      } // 4問目
+      else if (this.questionnamber === 3) {
+        this.string += ""
+        console.log(this.string)
+        this.questionnamber += 1
+      } // 5問目
+      else if (this.questionnamber === 4) {
+        this.string += "洋"
+        console.log(this.string)
         this.questionshow = false
         this.finishshow = true
       }
