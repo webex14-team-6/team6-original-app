500925486208fff8b6c7dc502a91a7e154f34528
yurina-minami <yurinatty373@gmail.com>
2022-06-14 19:58:59 +0900

    トップページのviewコンポーネントとrouterの設定

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
   <Header />
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>｜
-    <router-link to="/shindan">診断</router-link>
-  </nav>
-  <router-view />
-  <AlcholeCategory />
   <HamburgerMevu />
+  <AlcholeCategory />
 </template>
 
 <script>
-
 import Header from "./components/Header.vue"
 import HamburgerMevu from "./components/HamburgerMevu.vue"
 import AlcholeCategory from "./components/AlcholeCategory.vue"
@@ -31,19 +24,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  color: black;
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mainvisual">
+    <img src="../assets/mainvisual.png" alt="mainvisual" />
+  </div>
+  <div class="shindan-container">
+    <h2>オススメお酒診断</h2>
+  </div>
+  <div class="osusume-container">
+    <h2>お酒解説</h2>
+  </div>
+</template>
+
+<style scoped>
+.mainvisual img {
+  width: 100%;
+  height: 600px;
+  object-fit: cover;
+}
+
+.shindan-container {
+  width: 50%;
+  height: 400px;
+  background-color: gray; /* 識別するための仮の背景色 */
+}
+
+.osusume-container {
+  width: 50%;
+  height: 400px;
+  background-color: black; /* 識別するための仮の背景色 */
+}
+</style>
