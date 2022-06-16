12691e20ae2f1aedf9b6016860ca31d0d45337fb
yurina-minami <yurinatty373@gmail.com>
2022-06-16 11:06:57 +0900

    headerのデザイン

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <Header />
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
-  <AlcholeCategory />
 </template>
 
 <script>
-import Header from "./components/Header.vue"
-import AlcholeCategory from "./components/AlcholeCategory.vue"
+import Header from "./components/HeaderComponent.vue"
 
 export default {
   components: {
     Header,
-    AlcholeCategory,
   },
 }
 </script>
@@ -26,19 +19,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  color: black;
 }
 </style>
